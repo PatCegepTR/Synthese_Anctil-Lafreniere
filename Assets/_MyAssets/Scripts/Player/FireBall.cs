51cbd50caea5bc5edf2353636cf013f12cbf857e
Nathan Lafrenière <nathan.lafreniere.01@edu.cegeptr.qc.ca>
2023-05-15 15:23:03 -0400

    fireballhit

--- Assets/_MyAssets/Scripts/Player/FireBall.cs
+++ Assets/_MyAssets/Scripts/Player/FireBall.cs
@@ -6,12 +6,30 @@ public class FireBall : MonoBehaviour
 {
     [SerializeField] private float _vitesse = 14f;
 
+
+    private Animator _anim;
+
+
+    public void Start()
+    {
+        _anim = GetComponent<Animator>();
+    }
+
     void Update()
     {
         transform.Translate(Vector3.left * Time.deltaTime * _vitesse);
         if (transform.position.y > 60f)
         {
             Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Enemy")
+        {
+            _anim.SetBool("hit", true);
+            _vitesse = 0f;
         }
     }
 }
