4dd1d54976848e28f7796a3cf0c6041e4af20b7e
PatCegepTR <patrice.anctil.01@edu.cegeptr.qc.ca>
2023-05-15 15:23:44 -0400

    snake

---
+++ Assets/_MyAssets/Scripts/Snake.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Snake : MonoBehaviour
+{
+    [SerializeField] private float _vitesse = 7f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.left * Time.deltaTime * _vitesse);
+        TeleportationSerpent();
+    }
+
+    private void TeleportationSerpent()
+    {
+        if (transform.position.x > 41f)
+        {
+            float randomX = Random.Range(-0.56f, -2.12f);
+            transform.position = new Vector3(-40f, randomX, 0f);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (collision.tag == "FireBall")
+        {
+            UIManager uIManager = FindObjectOfType<UIManager>();
+            uIManager.AjouterScore(10);
+            Destroy(collision.gameObject);
+            Destroy(gameObject);
+
+        }
+
+        if (collision.tag == "ZoneEpee")
+        {
+            UIManager uIManager = FindObjectOfType<UIManager>();
+            uIManager.AjouterScore(10);
+            Destroy(collision.gameObject);
+            Destroy(gameObject);
+
+        }
+        if (collision.tag == "Player")
+        {
+            Player player = collision.transform.GetComponent<Player>();
+            player.Damage();
+
+            Destroy(gameObject);
+        }
+    }
+}
